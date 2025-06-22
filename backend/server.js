fbbbe6f43aef804b5b001c9c0912419b23d33bcb
Rahul Vimal <rviml358@gmail.com>
2025-06-22 12:09:19 +0530

    adding frontend on git

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import dotenv from "dotenv";
-import chats from "./data/data.js";
 import connectDB from "./config/db.js";
 import colors from "colors";
 import userRoutes from "./routes/userRoutes.js";
@@ -8,29 +7,45 @@ import chatRoutes from "./routes/chatRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
 import { Server } from "socket.io";
 import {notFound,errorHandler} from "./middlewares/errorMiddleware.js";
-import path from "path";
-dotenv.config();
+import path from 'path';
 
+dotenv.config();
 connectDB();
 const app=express();
 
 app.use(express.json()); //to accept json
 
-app.get('/',(req,res)=>{
-  res.send("API is Running SuccessFully!!");
-})
-
 app.use('/api/user',userRoutes);
 app.use("/api/chat",chatRoutes);
 app.use("/api/message",messageRoutes);
 
+const __dirname1 = path.resolve();
+if(process.env.NODE_ENV === 'producation')
+{
+app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+app.get("*",(req,res)=>{
+  res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+});
+}
+else
+{
+ app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+
+
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 4000;
-const server=app.listen(4000,()=>{
-    console.log(`Server Starting https://localhost:${PORT}`.yellow.bold);
-})
+const PORT = process.env.PORT;
+
+const server = app.listen(
+  PORT,
+  console.log(`Server running on PORT ${PORT}...`.yellow.bold)
+);
 
 const io = new Server(server, {
   pingTimeout: 60000,
@@ -51,6 +66,7 @@ socket.on('join chat', (room)=>{
 socket.join(room);
 console.log("User Join Roon" + room);
 });
+
 
 socket.on("typing", (room) => socket.in(room).emit("typing"));
 socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
