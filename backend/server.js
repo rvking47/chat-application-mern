0b5e95b921f3de2fa0fa722cbec54b7bf0c55496
Rahul Vimal <rviml358@gmail.com>
2025-06-22 18:36:16 +0530

    final chatapplicatio mern

--- backend/server.js
+++ backend/server.js
@@ -19,21 +19,23 @@ app.use('/api/user',userRoutes);
 app.use("/api/chat",chatRoutes);
 app.use("/api/message",messageRoutes);
 
-const __dirname1 = path.resolve();
-if(process.env.NODE_ENV === 'producation')
-{
-app.use(express.static(path.join(__dirname1, "/frontend/build")));
+// --------------------------deployment------------------------------
 
-app.get("*",(req,res)=>{
-  res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
-});
-}
-else
-{
- app.get("/", (req, res) => {
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("/", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
     res.send("API is running..");
   });
 }
+
+// --------------------------deployment------------------------------
 
 
 
