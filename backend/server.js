3707e2a21cd701ac1c07de5cfbb8d9ac2726d11e
Rahul Vimal <155147272+rvking47@users.noreply.github.com>
2025-07-16 16:45:18 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -54,7 +54,8 @@ const server = app.listen(
 const io = new Server(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+     origin: "*", 
+    credentials: true
   },
 });
 
